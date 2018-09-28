b8428834cf2672c56e2bc5372aef52952ae99e3a
geoffagnew <gagnew@gmail.com>
2018-09-27 21:25:48 -0400

    Commit before I build multi request fetch statement because this API is ridiculous.

--- src/components/StrainProfile/StrainProfile.js
+++ src/components/StrainProfile/StrainProfile.js
@@ -6,27 +6,37 @@ class StrainProfile extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      strain: null,
-      urlParam: this.props.match.params.id
+      strainDetails: null,
+      strainEffects: null,
+      strainFlavors: null,
+      urlParam: this.props.match.params.id,
+      isLoaded: false
     };
   }
 
   componentDidMount() {
+
     fetch(`http://strainapi.evanbusse.com/${ApiKey}/strains/search/name/${this.state.urlParam}`)
       .then((response) => response.json())
       // .then((data) => console.log(data))
-      .then((data) => this.setState({ strain: data }))
-      .catch((err) => console.log(`Something went wrong ${err}`));
+      .then(data => this.setState({ strainDetails: data, isLoaded: true }))
+      .catch(err => console.log(`Something went wrong ${err}`));
   }
 
   render() {
 
+    const { isLoaded, strainDetails } = this.state;
+
     let strainProfile;
 
-    if(this.state.strain !== null) {
-      strainProfile = this.state.strain[0].name;
-    } else {
+    if(!isLoaded) {
       strainProfile = 'Data is loading';
+    } else {
+      strainProfile = 
+        <div>
+          <p>{strainDetails[0].race}</p>
+          <p>{strainDetails[0].desc}</p>
+        </div>;
     }
 
     return (
@@ -38,10 +48,8 @@ class StrainProfile extends Component {
             </div>
           </div>
           <div className="row">
-            <div className="col-md-8 strain-list-wrapper p-4">
-              <ul className="strain-list pl-0">
+            <div className="col-md-8 strain-list-wrapper py-5 px-4">
                 {strainProfile}
-              </ul>
             </div>
             <div className="col-md-3 offset-md-1 strain-list-wrapper p-4">
               <p>Some cool as stuff goes here.</p>
