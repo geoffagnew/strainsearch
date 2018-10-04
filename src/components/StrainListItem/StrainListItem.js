201ac64810898e08f359c67c50e57d917a012055
geoffagnew <gagnew@gmail.com>
2018-10-04 11:53:12 -0400

    Added conditional logic to the effect/treatment text in the strain list. Now, the effect is prefixed according to whether the desired effect is recreational or medicinal in nature. I've also moved the arrays containing the rec and med effect listings to json files so they can be shared more easily across components.

--- src/components/StrainListItem/StrainListItem.js
+++ src/components/StrainListItem/StrainListItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import recOptions from '../../data/strain-options-rec.json';
 import StrainIcon from '../StrainIcon/StrainIcon';
 import './StrainListItem.scss';
 
@@ -9,7 +10,10 @@ const StrainListItem = (props) => (
     <li className="strain-list__item py-2 px-4">
       <h3 className="h6 mb-0 font-weight-bold">{props.strainName}</h3>
       <span className="strain-race"><StrainIcon strainName={props.strainRace} /> {props.strainRace}</span>
-      <span className="ml-2">Effect: {props.strainEffect}</span>
+      <span className="ml-1">
+        {recOptions.indexOf(props.strainEffect) !== -1 ? '— Desired effect: ' : '— Useful for treating '}
+        {props.strainEffect}
+      </span>
     </li>
   </NavLink>
 
