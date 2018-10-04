712b56804231488e08bac35b394f168d1b9b98ed
geoffagnew <gagnew@gmail.com>
2018-10-04 18:37:30 -0400

    Got the strain list filtering to work but there's a bug because this janky API has duplicates of the same item. This is causing React to render multiples of the same list component at times.

--- src/components/StrainsView/StrainsView.js
+++ src/components/StrainsView/StrainsView.js
@@ -7,31 +7,42 @@ import FilterButtonSmall from '../FilterButtonSmall/FilterButtonSmall';
 import { Route } from 'react-router-dom';
 import ApiKey from '../../config';
 import './StrainsView.scss';
+import { select } from 'glamor';
 
 class StrainsView extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      // strains collects all the strains from the api based on the effect value passed to the fetch request
       strains: [],
-      effectId: null
+      // effectId contains the effect value passed via the url params. This is used in the fetch request below
+      effectId: null,
+      strainVisible: 'all'
     };
   }
 
   // function to handle showing and hiding only the selected strains
   toggleStrains = (e) => {
-    const selectedFilter = e.target.textContent.toLowerCase();
-    // console.log(selectedFilter);
+    let selectedFilter = e.target.textContent.toLowerCase();
+    if (selectedFilter === 'reset') {
+      selectedFilter = 'all';
+    }
     this.setState({
-      strainToShow: 'all'
+      strainVisible: selectedFilter
     })
+  }
+
+  // function that calls the API based on the effect value and updates the local state
+  buildStrainList = (urlParams) => {
+    fetch(`http://strainapi.evanbusse.com/${ApiKey}/strains/search/effect/${urlParams}`)
+      .then((response) => response.json())
+      .then((data) => this.setState({ strains: data, effectId: urlParams }))
+      .catch((err) => console.log(`Something went wrong ${err}`));
   }
 
   componentDidMount() {
     const { id } = this.props.match.params;
-    fetch(`http://strainapi.evanbusse.com/${ApiKey}/strains/search/effect/${id}`)
-      .then((response) => response.json())
-      .then((data) => this.setState({ strains: data, effectId: id }))
-      .catch((err) => console.log(`Something went wrong ${err}`));
+    this.buildStrainList(id);
   }
 
   render() {
@@ -44,9 +55,33 @@ class StrainsView extends Component {
 
     // define strainsList variable to hold all strains from the array.map function below, and then displayed on the page
     let strainsList;
+    let selectedStrains;
 
-    if(this.state.strains.length > 0) {
-      strainsList = this.state.strains.map((strain) =>
+    if (this.state.strains.length > 0) {
+
+      if (this.state.strainVisible === 'sativa') {
+        selectedStrains = this.state.strains.filter(strain => {
+          if (strain.race === 'sativa') {
+            return true;
+          }
+        });
+      } else if (this.state.strainVisible === 'hybrid') {
+        selectedStrains = this.state.strains.filter(strain => {
+          if (strain.race === 'hybrid') {
+            return true;
+          }
+        });
+      } else if (this.state.strainVisible === 'indica') {
+        selectedStrains = this.state.strains.filter(strain => {
+          if (strain.race === 'indica') {
+            return true;
+          }
+        });
+      } else {
+        selectedStrains = this.state.strains.map((strain) => strain);
+      }
+
+      strainsList = selectedStrains.map((strain) =>
         <StrainListItem 
           key={strain.id}
           effectId={this.state.effectId}
@@ -73,6 +108,7 @@ class StrainsView extends Component {
                 <FilterButtonSmall btnText="Sativa" toggleStrains={this.toggleStrains} />
                 <FilterButtonSmall btnText="Indica" toggleStrains={this.toggleStrains} />
                 <FilterButtonSmall btnText="Hybrid" toggleStrains={this.toggleStrains} />
+                <FilterButtonSmall btnText="Reset" toggleStrains={this.toggleStrains} />
               <ul className="strain-list pl-0">
                 {strainsList}
               </ul>
