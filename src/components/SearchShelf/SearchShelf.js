d27a79208c7eec887b881af9b0c7f1b18d935b20
Geoff Agnew <gagnew@gmail.com>
2018-10-07 10:29:03 -0400

    Have the SearchShelf component now working properly with tabbed navigation. Still needs some CSS TLC.

--- src/components/SearchShelf/SearchShelf.js
+++ src/components/SearchShelf/SearchShelf.js
@@ -1,25 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import FilterBlock from './../FilterBlock/FilterBlock';
-// import recEffects from '../../data/strain-options-rec.json';
+import Tab from './../Tab/Tab';
+import recEffects from '../../data/strain-options-rec.json';
 import medEffects from '../../data/strain-options-med.json';
 import './SearchShelf.scss';
 
-const SearchShelf = (props) => {
-  return (
-    <div className="search-shelf-wrapper">
-      <div className="container">
-        <div className="row py-4">
-          <div className="col-md-12">
-            <FilterBlock 
-              filterHead={null}
-              filterSubhead="Find new strains"
-              filterOptions={medEffects}
-            />
+const recOptions = {
+  id: 'recreactional',
+  head: 'Find the marijuana strain to suit your mood',
+  options: recEffects
+}
+
+const medOptions = {
+  id: 'medicinal',
+  head: 'Find the right marijuana strain for what ails you',
+  options: medEffects
+}
+
+class SearchShelf extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedView: 'recreational',
+      selectedViewDetails: recOptions 
+    }
+  }
+
+  // event listener for toggling between tabs and displaying the relevant content in FilterBlock component
+  handleTabClick = (e) => {
+    e.preventDefault();
+    const id = e.target.dataset.id;
+    if(this.state.selectedView === 'recreational') {
+      this.setState({
+        selectedView: id,
+        selectedViewDetails: medOptions
+      });
+    } else {
+      this.setState({
+        selectedView: id,
+        selectedViewDetails: recOptions
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="search-shelf-wrapper">
+        <div className="container">
+          <div className="row py-4">
+            <div className="col-md-12">
+              <ul className="tabs mb-3">
+                <Tab tabText="Recreational effects" currentView={this.state.selectedView} handleTabClick={this.handleTabClick} id="recreational" />
+                <Tab tabText="Medicinal effects" currentView={this.state.selectedView} handleTabClick={this.handleTabClick} id="medicinal" />
+              </ul>
+              <FilterBlock 
+                filterHead={null}
+                filterSubhead={this.state.selectedViewDetails.head}
+                filterOptions={this.state.selectedViewDetails.options}
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default SearchShelf;
